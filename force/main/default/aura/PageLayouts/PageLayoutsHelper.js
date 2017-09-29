f8c02f65be4fc9ad171e376b85d25a5c7e4ee23d
David Frudd <dfrudd@financialforce.com>
2017-09-29 20:04:06 +0100

    Display messages in ui:message component

--- force/main/default/aura/PageLayouts/PageLayoutsHelper.js
+++ force/main/default/aura/PageLayouts/PageLayoutsHelper.js
@@ -1,30 +1,56 @@
 ({
 	updateLayout: function (component) {
 		var action = component.get('c.updateLayout'),
-            layoutRequest = {
-                Strategy: component.get('v.strategy'),
-                ObjectType: component.get('v.objectType'),
-                LayoutName: component.get('v.layoutName'),
-                Operation: component.get('v.operation'),
-                Behavior: component.get('v.behavior'),
-                Field: component.get('v.field'),
-                AnchorType: component.get('v.anchorType'),
-                AnchorField: component.get('v.anchorField')
-            };
+			layoutRequest = {
+				Strategy: component.get('v.strategy'),
+				ObjectType: component.get('v.objectType'),
+				LayoutName: component.get('v.layoutName'),
+				Operation: component.get('v.operation'),
+				Behavior: component.get('v.behavior'),
+				Field: component.get('v.field'),
+				AnchorType: component.get('v.anchorType'),
+				AnchorField: component.get('v.anchorField')
+			};
 
-        action.setParams({
-            requestString: JSON.stringify(layoutRequest)
-        });
+		component.set('v.message', '');
+		component.set('v.messageSeverity', '');
+
+		action.setParams({
+			requestString: JSON.stringify(layoutRequest)
+		});
 
 		action.setCallback(this, function (response) {
-			var state = response.getState();
-			if (state === "SUCCESS") {
-				component.set("v.response", response.getReturnValue());
-			} else if (state === "ERROR") {
-				component.set("v.response", "Error");
+			var state = response.getState(),
+				messageSeverity = 'Info',
+				message,
+				responseMessages;
+
+			if (state === 'SUCCESS') {
+				messageSeverity = 'Info';
+				responseMessages = response.getReturnValue();
+
+				if (responseMessages && responseMessages[0] && responseMessages[0].message) {
+					message = responseMessages[0].message;
+				} else {
+					message = 'Success';
+				}
+			} else if (state === 'ERROR') {
+				messageSeverity = 'Error';
+				responseMessages = response.getError();
+
+				if (responseMessages && responseMessages[0] && responseMessages[0].message) {
+					message = responseMessages[0].message;
+				} else {
+					message = 'Unknown error';
+				}
+
 			} else {
-				component.set("v.response", "Unknown state: " + state);
+				messageSeverity = 'Error';
+				component.set('v.response', 'Unknown state: ' + state);
 			}
+
+			component.set('v.message', message);
+			component.set('v.messageSeverity', messageSeverity);
 		});
 
 		$A.enqueueAction(action);
